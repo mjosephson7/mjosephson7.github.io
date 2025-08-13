bff5c26b610a7dee11b4fea2a62e15ec08fca9db
mjosephson717 <82902295+mjosephson717@users.noreply.github.com>
2025-08-12 22:46:29 -0400

    Updated gitignore

---
+++ projects/firstGame/config.js
@@ -0,0 +1,12 @@
+// Firebase Configuration - KEEP THIS FILE OUT OF GITHUB
+// Add this file to .gitignore to prevent exposing API keys
+
+window.firebaseConfig = {
+    apiKey: "AIzaSyC-V2EFdyr_6pBPZMV4ueTdOCzcXvbNa2I",
+    authDomain: "circlegame-c4013.firebaseapp.com",
+    projectId: "circlegame-c4013",
+    storageBucket: "circlegame-c4013.firebasestorage.app",
+    messagingSenderId: "1001062250382",
+    appId: "1:1001062250382:web:b19cedf327f40c11d356df",
+    measurementId: "G-DT8YNQZDEZ"
+};
